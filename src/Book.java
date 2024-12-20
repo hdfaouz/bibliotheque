d692495b2f40df3a3a2fa5481839f23666c9cd7f
hdfaouz <hdfaouzia2@gmail.com>
2024-12-20 18:02:41 +0100

    fonction rechercher et supprimer

--- src/Book.java
+++ src/Book.java
@@ -1,32 +1,123 @@
+import java.sql.SQLOutput;
 import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Book {
-    String titre;
-    String auteur;
-   String isdn;
-    boolean desponibilite;
-    ArrayList<Book>books = new ArrayList<Book>();
-
-void Ajouter() {
     Scanner scanner = new Scanner(System.in);
-    System.out.print("entrer le titre : ");
-     titre = scanner.nextLine();
-    System.out.print("entrer le nom de l'auteur : ");
-     auteur = scanner.nextLine();
-    System.out.print("entrer isdn :");
-     isdn = scanner.nextLine();
-    System.out.print("entrer la disponibilité  :");
-    desponibilite = scanner.nextBoolean();
+
+
+    private String titre;
+    private String auteur;
+    private String isbn;
+    private boolean desponibilite;
+    ArrayList<Book> books = new ArrayList<Book>();
+
+    //Constructeur
+    public Book(String titre, String auteur, String isbn, boolean disonibilte) {
+        this.titre = titre;
+        this.auteur = auteur;
+        this.isbn = isbn;
+        this.desponibilite = disonibilte;
+    }
+
+    Book() {
+    }
+
+
+    // getters
+    public String getTitre() {
+        return this.titre;
+    }
+
+    // setters
+    public void setTitre(String titre) {
+        this.titre = titre;
+    }
+
+    public String getAuteur() {
+        return this.auteur;
+    }
+
+    public void setAuteur(String auteur) {
+        this.auteur = auteur;
+
+    }
+
+    public String getIsbn() {
+        return this.isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public boolean getDesponibilite() {
+        return this.desponibilite;
+    }
+
+    public void setDesponibilite(Boolean desponibilite) {
+        this.desponibilite = desponibilite;
+    }
+
+
+    // CRUD functions
+    void ajouter() {
+
+        System.out.print("entrer le titre : ");
+        titre = scanner.nextLine();
+        System.out.print("entrer le nom de l'auteur : ");
+        auteur = scanner.nextLine();
+        System.out.print("entrer isbn :");
+        isbn = scanner.nextLine();
+        System.out.print("entrer la disponibilité oui  :");
+        desponibilite = scanner.nextBoolean();
+        books.add(new Book(titre, auteur, isbn, desponibilite));
+    }
+
+    public void afficher() {
+        System.out.println("le titre " + titre);
+        System.out.println("l'auter " + auteur);
+        System.out.println("isdn " + isbn);
+        System.out.println("desponibilite " + desponibilite);
+    }
+
+    public void afficherts() {
+        for (int i = 0; i < books.size(); i++) {
+            books.get(i).afficher();
+        }
+    }
+
+
+    public Book recherche() {
+        System.out.println("entrer l'isbn  : ");
+        String isbn=scanner.nextLine();
+        for (Book book : books) {
+            if (book.getIsbn().equals(isbn)) {
+                return book;
+            } else {
+                System.out.println("isbn non trouvé");
+            }
+        }
+        return null;
+
+        }
+    public void supprimer() {
+
+        Book book = recherche();
+        if (book != null){
+            books.remove(book);
+        }
+        else System.out.println("book non trouvé");
+
+    }
 }
-public void Afficher(){
-    System.out.println("le titre " +titre);
-    System.out.println("l'auter " +auteur);
-    System.out.println("isdn " +isdn);
-    System.out.println("desponibilite " +desponibilite);
-}
 
 
 
 
-}
+
+
+
+
+
+
--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,32 @@
+import java.sql.SQLOutput;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-       Book book = new Book();
-       book.Ajouter();
-       book.Afficher();
+        Book book = new Book();
+
+        int choix = 6;
+        System.out.println("1-Ajoutter un livre : ");
+        System.out.println("2- Afficher les livre :");
+        System.out.println("3- Rechercher un livre : ");
+        System.out.println("4- Modifier un livre: ");
+        System.out.println(" 5- Supprimer un livre : ");
+        System.out.println("6- Quitter");
+        System.out.println("entrer un choix entre 1 et 6");
+        Scanner scanner = new Scanner(System.in);
+        choix = scanner.nextInt();
+        switch (choix) {
+            case 1:
+                book.ajouter();
+                break;
+            case 2:
+                book.afficher();
+                break;
+            case 3:
+                book.recherche();
+            case 4 :
+                book.supprimer();
+        }
     }
 }
-
-
-
-
 
